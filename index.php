6b67af4a1c004350f188c001e9bf4e318f911d26
PR12 <PR12@figd-1711712.figdtk.de>
2018-03-14 09:24:57 +0100

    init

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>PHP 01 BASIC</title>
+        <meta name="viewport" content="width-divice-width, initial-scale=1.0">
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="assets/css/styles.css" rel="stylesheet" type="text/css"/>
+        <script src="assets/js/jquery-3.3.1.min.js" type="text/javascript"></script>
+        <script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="assets/js/main.js" type="text/javascript"></script>
+    </head>
+    <body>
+        <div class="container">
+
+                <p>Hallo</p>
+                
+                
+        </div>
+    </body>
+</html>
