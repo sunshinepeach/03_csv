d5303217cb411977be7d60916c4eb335b2ddfb58
PR12 <PR12@figd-1711712.figdtk.de>
2018-03-14 15:23:03 +0100

    csv datei auslesen und in tabelle anzeigen

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>PHP 01 BASIC</title>
+        <title>PHP 03 CSV</title>
         <meta name="viewport" content="width-divice-width, initial-scale=1.0">
         <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
         <link href="assets/css/styles.css" rel="stylesheet" type="text/css"/>
@@ -11,11 +11,89 @@
         <script src="assets/js/main.js" type="text/javascript"></script>
     </head>
     <body>
-        <div class="container">
 
-                <p>Hallo</p>
-                
-                
+
+        <div class="container">
+            <h3>CSV-Daten importieren und exportieren</h3>
+            <?php
+            // 
+            $handle = false; // Zeiger
+            // articles_mit_quotes.csv
+            // articles.csv
+            $filename = 'articles_mit_quotes.csv'; 
+            $folder = './import/';
+            if (file_exists($folder . $filename)) {
+                // Dateizeilen-Zeiger
+                $handle = fopen($folder . $filename, "r");
+                // Eine Zeile auslesen
+                //$row1 = fgetcsv($handle, 0, ';');
+                //$row2 = fgetcsv($handle, 0, ';');
+                // alle Zeilen auslesen
+                // erste Zeile --> Überschriften
+                $columnNames = fgetcsv($handle, 0, ';');
+                // restliche Zeilen in ein Array packen --> 2dim array
+                $rows = [];
+                while ($row = fgetcsv($handle, 0, ';')) {
+                    //echo $row[0] . '<br>';
+                    $rows[] = $row;
+                }
+            }
+            ?>
+            <table class="table table-striped table-hover" ><?php ?>
+                <thead>
+                    <tr>
+                        <!-- echo %columnNames[x] -->
+                        <th><?php echo $columnNames[3]; ?></th>
+                        <th><?php echo $columnNames[5]; ?></th>
+                        <th><?php echo $columnNames[6]; ?></th>
+                        <th><?php echo $columnNames[11]; ?></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <!-- Schleife Anfang $rows -->
+                    <!-- vgl. mit for-Schleife -->
+                    <?php foreach ($rows AS $article) : ?>
+                        <tr> <!-- echo $rows[x][y] -> echo $row[y]-->
+                            <td><?php echo $article[3]; ?></td>
+                            <td><?php echo $article[5]; ?></td>
+                             <td><?php echo $article[6]; ?></td>
+                            <td><?php echo $article[11]; ?></td>
+                        </tr>
+                        <!-- Schleife Ende -->
+                    <?php endforeach; ?>
+                </tbody> 
+            </table>
+            
+            <!-- Gesamte Tabelle ausgeben  -->
+            <h4>Table full with foreach and for </h4>
+            <table class="table table-striped table-hover" ><?php ?>
+                <thead>
+                    <tr>
+                        <?php for ($i = 0; $i < count($columnNames); $i++ ) : ?>
+                            <th><?php echo $columnNames[$i]; ?></th>
+                        <?php endfor; ?>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($rows AS $article) : ?>
+                    <tr> 
+                        <?php for ($i = 0; $i < count($article); $i++ ) : ?>
+                            <td><?php echo $article[$i]; ?></td>
+                        <?php endfor; ?>                   
+                    </tr>
+                     <?php endforeach; ?> 
+                   
+                </tbody> 
+            </table> 
+            
         </div>
+        <hr>
+        <pre>
+            <?php
+            //print_r($rows);
+            //var_dump($row2);
+            ?>
+        </pre>
+
     </body>
 </html>
